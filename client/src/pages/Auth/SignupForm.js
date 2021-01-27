408176442b849c1e1ad488f2a31256a5bacbe549
jhf1203 <jhf1203@gmail.com>
2021-01-26 20:48:21 -0500

    styled register page

--- client/src/pages/Auth/SignupForm.js
+++ client/src/pages/Auth/SignupForm.js
@@ -48,52 +48,65 @@ function SignupForm() {
   return (
     <Container>
       <Row>
-        <Col size="md-3"></Col>
-        <Col size="md-6">
-          <Card title="Register for React Reading List">
-            <form style={{marginTop: 10}}>
-              <label htmlFor="username">First name: </label>
-              <Input
-                type="text"
-                name="firstName"
-                value={userObject.firstName}
-                onChange={handleChange}
-              />
-              <label htmlFor="username">Last name: </label>
-              <Input
-                type="text"
-                name="lastName"
-                value={userObject.lastName}
-                onChange={handleChange}
-              />
-              <label htmlFor="username">Username: </label>
-              <Input
-                type="text"
-                name="username"
-                value={userObject.username}
-                onChange={handleChange}
-              />
-              <label htmlFor="password">Password: </label>
-              <Input
-                type="password"
-                name="password"
-                value={userObject.password}
-                onChange={handleChange}
-              />
-              <label htmlFor="confirmPassword">Confirm Password: </label>
-              <Input
-                type="password"
-                name="confirmPassword"
-                value={userObject.confirmPassword}
-                onChange={handleChange}
-              />
-              <Link to="/">Login</Link>
-              <FormBtn onClick={handleSubmit}>Register</FormBtn>
-            </form>
-          </Card>
-        </Col>
-        <Col size="md-3"></Col>
+        {/* <Col size="md-1"></Col> */}
+        <div className="col-md-12 outer-col">
+          <div className="row inner-row">
+          <div className="col-md-4 inner-col">
+            <div className="row register-title-row">
+              <h2 className="signup-title-text">register</h2>
+            </div>
+            <div className="row signup-form-row">
+                <div className="card register-card">
+                  <form style={{marginTop: 10}}>
+                    <input className="signup-input"
+                      placeholder="first name"
+                      type="text"
+                      name="firstName"
+                      value={userObject.firstName}
+                      onChange={handleChange}
+                    />
+                    <input className="signup-input"
+                      placeholder="last name"
+                      type="text"
+                      name="lastName"
+                      value={userObject.lastName}
+                      onChange={handleChange}
+                    />
+                    <input className="signup-input"
+                      placeholder="username"
+                      type="text"
+                      name="username"
+                      value={userObject.username}
+                      onChange={handleChange}
+                    />
+                    <input className="signup-input"
+                      placeholder="password"
+                      type="password"
+                      name="password"
+                      value={userObject.password}
+                      onChange={handleChange}
+                    />
+                    <input className="signup-input"
+                      placeholder="confirm password"
+                      type="password"
+                      name="confirmPassword"
+                      value={userObject.confirmPassword}
+                      onChange={handleChange}
+                    />
+                    <Link className="btn btn-link login-btn mb-4" to="/">Login</Link>
+                    <button className="btn btn-link signup-btn mb-4 mr-5" onClick={handleSubmit}>Register</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <Col size="md-4"></Col>
+            
+            <Col size="md-4"></Col>
+          </div>
+        </div>
+        {/* <Col size="md-1"></Col> */}
       </Row>
+      
     </Container>
   )
 }
